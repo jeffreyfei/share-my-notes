8259b8a54a724c8747c14f0b9dd4212849114749
Jeffrey Fei <jeffreyfei@msn.com>
2017-11-12 21:48:59 -0500

    Add build script / Update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# share-my-note
+# Share  My Notes
+- Realtime note sharing platform
+
+# Development
+### Build server
+```
+bash dev.sh build
+```
+### Run server
+```
+bash dev.sh run-server
+```
---
+++ dev.sh
@@ -0,0 +1,10 @@
+if [ $1 == "build" ]; then
+    mkdir -p bin
+    pushd bin
+        go build ../server/cmd/share-my-notes
+    popd
+elif [ $1 == "run-server" ]; then
+    ./bin/share-my-notes
+else
+    echo "Unrecognized command"
+fi
