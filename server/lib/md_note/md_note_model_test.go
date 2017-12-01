240f05c004318752a7fa18a06536bcd76786116c
Jeffrey Fei <jeffreyfei@msn.com>
2017-12-01 03:56:50 -0500

    Finish implementing md note model

---
+++ server/lib/md_note/md_note_model_test.go
@@ -0,0 +1,77 @@
+package md_note
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/jeffreyfei/share-my-notes/server/lib/db"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type MDNoteTestSuite struct {
+	suite.Suite
+	db *gorm.DB
+}
+
+func createMockNote(id, ownerID int64) MDNoteModel {
+	note := MDNoteModel{}
+	note.ID = id
+	note.OwnerID = ownerID
+	note.Category = fmt.Sprintf("mock-cat-%d", id)
+	note.RawText = fmt.Sprintf("mock-raw-text-%d", id)
+	note.CompiledText = fmt.Sprintf("mock-comp-text-%d", id)
+	note.CreatedAt = time.Now()
+	note.UpdatedAt = time.Now()
+	return note
+}
+
+func TestMDNoteTestSuite(t *testing.T) {
+	s := new(MDNoteTestSuite)
+	var err error
+	s.db, err = db.GetDB()
+	assert.NoError(t, err)
+	assert.NoError(t, AutoMigrate(s.db))
+	suite.Run(t, s)
+}
+
+func (s *MDNoteTestSuite) SetupTest() {
+	s.clearData()
+}
+
+func (s *MDNoteTestSuite) TearDownTest() {
+	s.clearData()
+}
+
+func (s *MDNoteTestSuite) clearData() {
+	assert.NoError(s.T(), s.db.Exec("TRUNCATE TABLE md_note_models").Error)
+}
+
+func (s *MDNoteTestSuite) TestNew() {
+	assert.NoError(s.T(), New(s.db, int64(1), "mock-cat", "### Test"))
+	var newNote MDNoteModel
+	assert.NoError(s.T(), s.db.First(&newNote).Error)
+	assert.Equal(s.T(), int64(1), newNote.OwnerID)
+	assert.Equal(s.T(), "mock-cat", newNote.Category)
+	assert.Equal(s.T(), "### Test", newNote.RawText)
+	assert.Equal(s.T(), "<h3><a href=\"#test\" rel=\"nofollow\"><span></span></a>Test</h3>\n", newNote.CompiledText)
+}
+
+func (s *MDNoteTestSuite) TestUpdate() {
+	note := createMockNote(int64(1), int64(1))
+	assert.NoError(s.T(), s.db.Create(&note).Error)
+	assert.NoError(s.T(), Update(s.db, int64(1), "### Test"))
+	var savedNote MDNoteModel
+	assert.NoError(s.T(), s.db.First(&savedNote, 1).Error)
+	assert.Equal(s.T(), "<h3><a href=\"#test\" rel=\"nofollow\"><span></span></a>Test</h3>\n", savedNote.CompiledText)
+}
+
+func (s *MDNoteTestSuite) TestDelete() {
+	note := createMockNote(int64(1), int64(1))
+	assert.NoError(s.T(), s.db.Create(&note).Error)
+	assert.NoError(s.T(), Delete(s.db, int64(1)))
+	var deletedNote MDNoteModel
+	assert.True(s.T(), s.db.First(&deletedNote, 1).RecordNotFound())
+}
--- server/lib/user/user_model_test.go
+++ server/lib/user/user_model_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/jeffreyfei/share-my-notes/server/lib/db"
+	"github.com/jeffreyfei/share-my-notes/server/lib/md_note"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -16,9 +17,16 @@ type UserModelTestSuite struct {
 	db *gorm.DB
 }
 
-func createMockUser(id string) UserModel {
+func createMockNote(ownerID int64) md_note.MDNoteModel {
+	note := md_note.MDNoteModel{}
+	note.OwnerID = ownerID
+	return note
+}
+
+func createMockUser(id int64, googleID string) UserModel {
 	user := UserModel{}
-	user.GoogleID = id
+	user.ID = id
+	user.GoogleID = googleID
 	user.Name = fmt.Sprintf("mock-name-%s", id)
 	user.ImageURL = fmt.Sprintf("mock-image-url-%s", id)
 	user.CreatedAt = time.Now()
@@ -36,11 +44,16 @@ func TestUserModelTestSuite(t *testing.T) {
 }
 
 func (s *UserModelTestSuite) SetupTest() {
-	assert.NoError(s.T(), s.db.Exec("DELETE FROM user_models").Error)
+	s.clearData()
+}
+
+func (s *UserModelTestSuite) clearData() {
+	assert.NoError(s.T(), s.db.Exec("TRUNCATE TABLE user_models").Error)
+	assert.NoError(s.T(), s.db.Exec("TRUNCATE TABLE md_note_models").Error)
 }
 
 func (s *UserModelTestSuite) TestNewUser() {
-	user := createMockUser("1")
+	user := createMockUser(int64(1), "1")
 	assert.NoError(s.T(), NewUser(s.db, &user))
 	var savedUser UserModel
 	assert.NoError(s.T(), s.db.First(&savedUser).Error)
@@ -50,7 +63,7 @@ func (s *UserModelTestSuite) TestNewUser() {
 }
 
 func (s *UserModelTestSuite) TestHandleLoginNewUser() {
-	user := createMockUser("1")
+	user := createMockUser(int64(1), "1")
 	returnedUser, err := HandleLogin(s.db, &user)
 	assert.NoError(s.T(), err)
 	assert.Equal(s.T(), user.GoogleID, returnedUser.GoogleID)
@@ -64,7 +77,7 @@ func (s *UserModelTestSuite) TestHandleLoginNewUser() {
 }
 
 func (s *UserModelTestSuite) TestHandleLoginExistingUser() {
-	user := createMockUser("1")
+	user := createMockUser(int64(1), "1")
 	assert.NoError(s.T(), s.db.Save(&user).Error)
 	returnedUser, err := HandleLogin(s.db, &user)
 	assert.NoError(s.T(), err)
@@ -76,4 +89,25 @@ func (s *UserModelTestSuite) TestHandleLoginExistingUser() {
 	assert.Equal(s.T(), user.GoogleID, savedUser.GoogleID)
 	assert.Equal(s.T(), user.Name, savedUser.Name)
 	assert.Equal(s.T(), user.ImageURL, savedUser.ImageURL)
+}
+
+func (s *UserModelTestSuite) TestMDNotes() {
+	user1 := createMockUser(int64(1), "1")
+	user2 := createMockUser(int64(2), "2")
+	note1 := createMockNote(int64(1))
+	note2 := createMockNote(int64(1))
+	note3 := createMockNote(int64(1))
+	note4 := createMockNote(int64(2))
+	note5 := createMockNote(int64(2))
+	assert.NoError(s.T(), s.db.Create(&note1).Error)
+	assert.NoError(s.T(), s.db.Create(&note2).Error)
+	assert.NoError(s.T(), s.db.Create(&note3).Error)
+	assert.NoError(s.T(), s.db.Create(&note4).Error)
+	assert.NoError(s.T(), s.db.Create(&note5).Error)
+	notes, err := user1.MDNotes(s.db)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 3, len(notes))
+	notes, err = user2.MDNotes(s.db)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 2, len(notes))
 }
